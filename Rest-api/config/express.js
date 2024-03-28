41b3cfd872bb1032c2d5ee51ed5da0b409423088
Todor Mitev <td_mitev@abv.bg>
2024-03-28 14:40:14 +0200

    Install express-session

--- Rest-api/config/express.js
+++ Rest-api/config/express.js
@@ -3,11 +3,20 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const cookieSecret = process.env.COOKIESECRET || 'TdMitev';
 const { errorHandler } = require('../utils')
+const session = require('express-session');
 
 module.exports = (app) => {
     app.use(express.json());
 
     app.use(cookieParser(cookieSecret));
+
+    app.use(session({
+        secret: 'odkf0sd9u_90asi09w3w9%$#wiudYG&6w%$#fty', 
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false } 
+      }));
+  
 
     app.use(express.static(path.resolve(__basedir, 'static')));
 
