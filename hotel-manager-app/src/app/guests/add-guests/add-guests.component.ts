a8e1da29baa9280e1ef77dc874bf1cbd37d9f0ad
Todor Mitev <td_mitev@abv.bg>
2024-04-02 21:47:49 +0300

    Created add-guests component

---
+++ hotel-manager-app/src/app/guests/add-guests/add-guests.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-guests',
+  templateUrl: './add-guests.component.html',
+  styleUrls: ['./add-guests.component.css']
+})
+export class AddGuestsComponent {
+
+}
--- hotel-manager-app/src/app/guests/guests.module.ts
+++ hotel-manager-app/src/app/guests/guests.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GuestsRoutingModule } from './guests-routing.module';
+import { AddGuestsComponent } from './add-guests/add-guests.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddGuestsComponent
+  ],
   imports: [
     CommonModule,
     GuestsRoutingModule
