fde11b22eb93db5be4d46b13d60d15a1ac3572ac
Todor Mitev <td_mitev@abv.bg>
2024-04-03 20:48:07 +0300

    Update home page styling

--- hotel-manager-app/src/app/home/home-page/home-page.component.ts
+++ hotel-manager-app/src/app/home/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeIn } from 'src/app/animations/animations';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  animations: [fadeIn]  
 })
 export class HomePageComponent implements OnInit{
 
