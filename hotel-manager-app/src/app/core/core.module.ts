e1022fb713b42b64a3aa2b6d949514f733d41023
Todor Mitev <td_mitev@abv.bg>
2024-06-08 14:55:09 +0300

    chore: Add ChatComponent to CoreModule and update environment files

--- hotel-manager-app/src/app/core/core.module.ts
+++ hotel-manager-app/src/app/core/core.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { ChatComponent } from './chat/chat.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ChatComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ChatComponent
   ]
 })
 export class CoreModule { }
--- hotel-manager-app/src/environments/environment.development.ts
+++ hotel-manager-app/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     apiUrl: 'http://localhost:3001/api',
+    flaskApiUrl: 'http://localhost:5000/api'
   };
   
